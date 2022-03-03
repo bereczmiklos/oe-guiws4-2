2d6f880f28a8eed8a7de7a69e9da43953e57a100
Berecz Miklós <bereczmiklos@stud.uni-obuda.hu>
2022-03-03 21:09:21 +0100

    Deleting branch

---
+++ oe-guiws4-2/Converters/EnumToCollectionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace oe_guiws4_2.Converters
+{
+    public class EnumToCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
