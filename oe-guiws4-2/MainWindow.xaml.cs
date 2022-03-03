d1de0bba9003566d52e064902f9753f6854e3fa0
nagymatyas@stud.uni-obuda.hu <nagymatyas@stud.uni-obuda.hu>
2022-03-03 14:43:58 +0100

    Serialization added

--- oe-guiws4-2/MainWindow.xaml.cs
+++ oe-guiws4-2/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using oe_guiws4_2.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +26,23 @@ namespace oe_guiws4_2
         public MainWindow()
         {
             InitializeComponent();
+
+            if (File.Exists("heroes.json"))
+            {
+                var heroes = JsonConvert.DeserializeObject<Hero[]>(File.ReadAllText("heroes.json"));
+                heroes.ToList().ForEach(x => lb_barrack.Items.Add(x));
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            List<Hero> hero = new List<Hero>();
+            foreach (var item in lb_barrack.Items)
+            {
+                hero.Add(item as Hero);
+            }
+            string jsonData = JsonConvert.SerializeObject(hero);
+            File.WriteAllText("heroes.txt", jsonData);
         }
     }
 }
