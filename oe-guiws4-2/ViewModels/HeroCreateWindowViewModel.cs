abb7e5d458632f3f4b3ebafd64f3d685dd0bbc45
nagymatyas@stud.uni-obuda.hu <nagymatyas@stud.uni-obuda.hu>
2022-03-03 22:06:14 +0100

    HeroCreateWindowViewModel added

---
+++ oe-guiws4-2/ViewModels/HeroCreateWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using oe_guiws4_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oe_guiws4_2.ViewModels
+{
+    public class HeroCreateWindowViewModel
+    {
+        public Hero ActualHero { get; set; }
+
+        public void Setup(Hero hero)
+        {
+            this.ActualHero = hero;
+        }
+
+        public HeroCreateWindowViewModel()
+        {
+
+        }
+    }
+}
