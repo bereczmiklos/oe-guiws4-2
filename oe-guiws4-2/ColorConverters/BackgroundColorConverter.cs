4a6ef70f42390a2d3dfb03a6cdb7addca5b71fb3
nagymatyas@stud.uni-obuda.hu <nagymatyas@stud.uni-obuda.hu>
2022-03-03 13:59:46 +0100

    ColorConverters added

---
+++ oe-guiws4-2/ColorConverters/BackgroundColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace oe_guiws4_2.ColorConverters
+{
+    public class BackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string herotypes = value.ToString();
+            if (herotypes == "Good")
+            {
+                return Brushes.LightGreen;
+            }
+            if (herotypes == "Evil")
+            {
+                return Brushes.Red;
+            }
+            return Brushes.LightYellow;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ oe-guiws4-2/ColorConverters/ForegroundColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace oe_guiws4_2.ColorConverters
+{
+    public class ForegroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double number = double.Parse(value.ToString());
+            if (number <= 3)
+            {
+                return Brushes.DarkRed;
+            }
+            else if (number <= 7)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
